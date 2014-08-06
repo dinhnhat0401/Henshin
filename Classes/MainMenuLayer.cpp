5f3d2c67652e98bc4349d0ff9dd961005b78023f
Đinh Văn Nhật <nhatphutho@gmail.com>
2014-08-07 03:09:15 +0900

    add project file and delete unessesary files

--- Classes/MainMenuLayer.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  MainMenuLayer.cpp
-//  Henshin
-//
-//  Created by duong minh khoa on 7/30/14.
-//  Copyright (c) 2014 (THE) ONE of THEM. All rights reserved.
-//
-
-#include "MainMenuLayer.h"
